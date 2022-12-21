450be023e1ece76d66bf7057edea2d9531fbb8e0
Rayzek <ian.rumiche@pucp.edu.pe>
2022-12-21 17:39:43 -0500

    Cambios

--- PROYECTO_SPARK/prueba.py
+++
@@ -1,7 +0,0 @@
-# For running Spark
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName("Spark").getOrCreate()
-# For running Pandas on top of Spark
-import pyspark.pandas as ps
-
-a=ps.DataFrame()
