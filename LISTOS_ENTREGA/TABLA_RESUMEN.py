3a69ac6f360555957d92726246aa128b6ada431b
Rayzek <ian.rumiche@pucp.edu.pe>
2023-01-09 20:56:52 -0500

    Cambios

--- LISTOS_ENTREGA/TABLA_RESUMEN.py
+++ LISTOS_ENTREGA/TABLA_RESUMEN.py
@@ -13,7 +13,40 @@ anterior = gsheet.worksheet("Anterior").get_all_records()
 base1= pd.DataFrame(anterior)
 
 # INSTAURAMOS EL CONTROLADOR DE PESTAÑA
-wsheet = gsheet.worksheet("Final")
+wsheet = gsheet.worksheet("Final 2022")
+
+#departamentos=base1["Departamento"]
+#base1=base1.drop(["Departamento"], axis=1)
+#base1=base1.astype("int")
+
+base1=base1.rename(columns={'1': '01',"2":"02","3":"03","4":"04","5":"05","6":"06","7":"07","8":"08","9":"09"})
+base2=base2.rename(columns={'1': '01',"2":"02","3":"03","4":"04","5":"05","6":"06","7":"07","8":"08","9":"09"})
+
+base1 = base1.set_index('Departamento').cumsum(axis=1)
+base2 = base2.set_index('Departamento').cumsum(axis=1)
+
+data=base2-base1
+data=data.reset_index()
+data=data.fillna("")
+data=data.sort_values(["18", "17"], ascending=[True, True])
+#data.insert(0,"Departamentos",departamentos)
+wsheet.update([data.columns.values.tolist()]+data.values.tolist())
+
+
+
+
+
+
+
+################################
+actual = gsheet.worksheet("DIC-2021").get_all_records()
+base2= pd.DataFrame(actual)
+
+anterior = gsheet.worksheet("NOV-2021").get_all_records()
+base1= pd.DataFrame(anterior)
+
+# INSTAURAMOS EL CONTROLADOR DE PESTAÑA
+wsheet = gsheet.worksheet("Final 2021")
 
 #departamentos=base1["Departamento"]
 #base1=base1.drop(["Departamento"], axis=1)
