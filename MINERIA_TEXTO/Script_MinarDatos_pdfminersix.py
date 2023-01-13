bde92fc9408f8ae87a929685b54c5a12f344568e
Rayzek <ian.rumiche@pucp.edu.pe>
2023-01-13 00:36:27 -0500

    Cambios

--- MINERIA_TEXTO/Script_MinarDatos_pdfminersix.py
+++
@@ -1,9 +0,0 @@
-from pdfminer.high_level import extract_text
-
-#https://pypi.org/project/pdfminer.six/
-archivo=(r"H:\Mi unidad\TRABAJO\MINERÍA DE DATOS\EJEMPLOS\EJEMPLO MINERIA DE DATOS\CVS\CV_NATHAN.pdf")
-
-text = extract_text(archivo)
-print(text)
-if "data" in text:
-    print("Es el candidato")
