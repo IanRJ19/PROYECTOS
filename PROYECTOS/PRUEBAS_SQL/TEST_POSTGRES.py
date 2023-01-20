559cc34a350d19f22421e74ab7965c5812738a18
Rayzek <ian.rumiche@pucp.edu.pe>
2023-01-20 01:46:01 -0500

    Cambios

--- PROYECTOS/PRUEBAS_SQL/TEST_POSTGRES.py
+++ PROYECTOS/PRUEBAS_SQL/TEST_POSTGRES.py
@@ -1,25 +1,27 @@
-import psycopg2
+
 import pandas as pd
 from decouple import config
+from sqlalchemy import create_engine
 
 # Es importante habilitar mi ip en las redes permitidas
 # Obtén los detalles de conexión desde la configuración de la instancia de Cloud SQL
 
-connection = psycopg2.connect(
-        host=config("URL_POSTGRES_GCP"),
-        port=5432,
-        user=config("USER_POSTGRES_GCP"),
-        password=config("PASS_POSTGRES_GCP"),
-        dbname="postgres"
-)
+DB_USER=config("USER_POSTGRES_GCP")
+DB_PASSWORD=config("PASS_POSTGRES_GCP")
+DB_NAME="postgres"
+DB_PORT="5432"
+DB_HOST=config("URL_POSTGRES_GCP")
+
+cadena  = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+engine= create_engine(cadena,echo=True)
+
+print('La conexión está correcta')
+
 
 data={'name': ['John', 'Mike', 'Emily'],
         'age': [25, 30, 35],
         'city': ['New York', 'Los Angeles', 'Chicago']}
 df = pd.DataFrame(data)
-df.to_sql("Iris",schema='public', con=connection, if_exists="append", index=False)
-# Guardar los cambios en la base de datos
-connection.commit()
-
-# Cerrar la conexión
-connection.close()
+df.to_sql("Iris", con=engine, if_exists="append", index=False)
