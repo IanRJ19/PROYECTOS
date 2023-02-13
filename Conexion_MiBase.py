55b7d095b0030ec3747baa877656739a9594e548
Rayzek <ian.rumiche@pucp.edu.pe>
2023-02-13 00:44:47 -0500

    Cambios

--- Conexion_MiBase.py
+++ Conexion_MiBase.py
@@ -2,6 +2,8 @@ import pandas as pd
 from decouple import config
 from sqlalchemy import create_engine
 
+df=pd.read_csv('DATASETS\Iris.csv')
+
 DB_USER='postgres'
 DB_PASSWORD='cetroian'
 DB_NAME="Datasets"
@@ -13,4 +15,7 @@ connection = engine.connect()
 
 #base = pd.read_sql_query(f"SELECT * FROM public.mseg", con=connection)
 
+df.to_sql("Iris", con=engine, if_exists="append", index=False)
 connection.close()
+
+
