7e04df7080a419137c48b4b6da2d962fccbdc83d
Ian Marco Antonio Rumiche Juarez <ianrj19@gmail.com>
2023-07-16 23:22:52 -0500

    Cambios

--- FLASK/static/script.js
+++
@@ -1,9 +0,0 @@
-// archivo: static/script.js
-document.querySelector("form").addEventListener("submit", function(event) {
-    var guess = parseInt(document.querySelector("input[name='guess']").value, 10);
-    if (isNaN(guess) || guess < 1 || guess > 100) {
-      alert("Por favor, ingrese un número válido entre 1 y 100.");
-      event.preventDefault();
-    }
-  });
-  
